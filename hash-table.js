3b4dab60de9e05857ac897b3e2b1e98c6a3e8b8c
Joseph Cassell <yosefcassell@gmail.com>
2023-08-09 16:29:07 -0700

    part 1 and 2

--- hash-table.js
+++ hash-table.js
@@ -10,20 +10,31 @@ class KeyValuePair {
 
 class HashTable {
 
-  constructor(numBuckets = 4) {
-    // Your code here
+  constructor(numBuckets = 4)  {
+   this.count = 0
+   this.capacity = numBuckets
+   this.data = new Array(numBuckets).fill(null)
   }
 
   hash(key) {
-    // Your code here
+    let hexHash = sha256(key).slice(0,8)
+    return parseInt(`0x${hexHash}`)
   }
 
   hashMod(key) {
-    // Your code here
+    return this.hash(key) % this.capacity
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    const collision = new KeyValuePair(key, value)
+    const index = this.hashMod(key)
+    if(this.data[index]) {
+      throw new Error('hash collision or same key/value pair already exists!')
+    } else {
+      this.data[index] = collision;
+      this.count++
+
+    }
   }
 
   insertWithHashCollisions(key, value) {
